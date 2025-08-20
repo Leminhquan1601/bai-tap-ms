0786448e698a43ecad85b79439990a0cc9381921
Leminhquan1601 <leminhquan16022009@gmail.com>
2025-08-20 20:38:55 +0700

    add
    add

--- Program.cs
+++ Program.cs
@@ -1,19 +1,93 @@
-﻿sing System;
+﻿﻿using System;
 
-namespace UsingOperator
+namespace LeapYearCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            float width;
-            float height;
-            Console.Write("Enter width: ");
-            width = float.Parse(Console.ReadLine());
-            Console.Write("Enter height: ");
-            height = float.Parse(Console.ReadLine());
-            float area = width * height;
-            Console.WriteLine("Area is: " + area);
+            int year;
+            bool isLeapYear = false;
+            Console.WriteLine("Enter a year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            /*            bool isDivisibleBy4 = year % 4 == 0;
+                        if (isDivisibleBy4)
+                        {
+                            bool isDivisibleBy100 = year % 100 == 0;
+                            if (isDivisibleBy100)
+                            {
+                                bool isDivisibleBy400 = year % 400 == 0;
+                                if (isDivisibleBy400)
+                                {
+                                    isLeapYear = true;
+                                }
+                            }
+                            else
+                            {
+                                isLeapYear = true;
+                            }
+                        }
+                        if (isLeapYear)
+                        {
+                            Console.WriteLine("{0} is a leap year", year);
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0} is NOT a leap year", year);
+                        }
+            */
+            /*            if (year % 4 == 0)
+                        {
+                            if (year % 100 == 0)
+                            {
+                                if (year % 400 == 0)
+                                {
+                                    isLeapYear = true;
+                                }
+                            }
+                            else
+                            {
+                                isLeapYear = true;
+                            }
+                        }
+
+                        if (isLeapYear)
+                        {
+                            Console.WriteLine("{0} is a leap year", year);
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0} is NOT a leap year", year);
+                        }
+            */
+            bool isDivisibleBy4 = year % 4 == 0;
+            if (isDivisibleBy4)
+            {
+                bool isDivisibleBy100 = year % 100 == 0;
+                if (isDivisibleBy100)
+                {
+                    bool isDivisibleBy400 = year % 400 == 0;
+                    if (isDivisibleBy400)
+                    {
+                        isLeapYear = true;
+                    }
+                }
+                else
+                {
+                    isLeapYear = true;
+                }
+            }
+
+            if (isLeapYear)
+            {
+                Console.WriteLine("{0} is a leap year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is NOT a leap year", year);
+            }
+
         }
     }
 }
