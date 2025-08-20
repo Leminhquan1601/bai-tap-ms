4b6ec84b8586c59ba24056e715a23fea6a325b6a
Leminhquan1601 <leminhquan16022009@gmail.com>
2025-08-20 21:07:54 +0700

    add
    add

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 
-namespace MinInArray
+namespace DeleteElementFromArray
 {
     class Program
     {
-        static int MinValue(int[] a)
+        static int[] DeleteElement(int[] arr, int x)
         {
-            int idx = 0;
-            for (int i = 1; i < a.Length; i++)
-                if (a[i] < a[idx]) idx = i;
-            return idx;
+            int indexDel = -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == x)
+                {
+                    indexDel = i;
+                    break;
+                }
+            }
+
+            if (indexDel == -1) 
+                return arr;
+
+            int[] newArr = new int[arr.Length - 1];
+            for (int i = 0; i < indexDel; i++)
+                newArr[i] = arr[i];
+            for (int i = indexDel; i < newArr.Length; i++)
+                newArr[i] = arr[i + 1];
+
+            return newArr;
         }
 
         static void Main(string[] args)
@@ -24,10 +40,17 @@ namespace MinInArray
                 arr[i] = int.Parse(Console.ReadLine());
             }
 
-            int index = MinValue(arr);
-            Console.WriteLine($"Vị trí nhỏ nhất: {index}");
-            Console.WriteLine($"Giá trị nhỏ nhất: {arr[index]}");
+            Console.Write("Nhập phần tử cần xoá: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int[] result = DeleteElement(arr, x);
+
+            Console.WriteLine("Mảng sau khi xoá:");
+            foreach (int val in result)
+                Console.Write(val + " ");
         }
     }
 }
+
+
 
